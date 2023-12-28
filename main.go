a2a7742d4583545e8ab37d8b8697192ddce1db83
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2023-12-28 19:13:44 +0900

    websocket通信を受け入れられるようになりました。

--- main.go
+++ main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-    // ハンドラ関数を作成
-    handler := func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, this is your Go server!")
-    }
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// クロスオリジンリクエストのチェック
+		return true
+	},
+}
 
-    // ハンドラを指定してサーバーを起動
-    http.HandleFunc("/", handler)
-    fmt.Println("Server is listening on port 8080...")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println("Server couldn't start:", err)
-    }
+func main() {
+	// WebSocket用のエンドポイントを作成
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("Server is listening on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Server couldn't start:", err)
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading to WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	// WebSocket接続の処理
+	for {
+		// メッセージの読み取り
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			break
+		}
+		fmt.Printf("Received message: %s\n", msg)
+
+		// メッセージの書き込み
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			fmt.Println("Error writing message:", err)
+			break
+		}
+	}
 }
