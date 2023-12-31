2e8623d9c3ac743c7be174ca07969e6ac7173539
charlie <62843186+charlie-to@users.noreply.github.com>
2023-12-31 15:04:34 +0900

    add aircraft class

--- main.go
+++ main.go
@@ -1,51 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/websocket"
+	"takahashi.qse.tohoku.ac.jp/atcGameProject/pkg/aircraft"
+	"takahashi.qse.tohoku.ac.jp/atcGameProject/pkg/msg"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		// クロスオリジンリクエストのチェック
-		return true
-	},
-}
-
 func main() {
-	// WebSocket用のエンドポイントを作成
-	http.HandleFunc("/ws", wsHandler)
-	fmt.Println("Server is listening on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Server couldn't start:", err)
-	}
-}
+	a := aircraft.NewAircraft([2]float64{1.0, 1.0}, 1)
+	a.PrintPosition()
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("Error upgrading to WebSocket:", err)
-		return
-	}
-	defer conn.Close()
-
-	// WebSocket接続の処理
-	for {
-		// メッセージの読み取り
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("Error reading message:", err)
-			break
-		}
-		fmt.Printf("Received message: %s\n", msg)
-
-		// メッセージの書き込み
-		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			fmt.Println("Error writing message:", err)
-			break
-		}
-	}
+	// サーバーを作成
+	messenger := msg.Messenger{}
+	messenger.CreateMessageServer()
 }
