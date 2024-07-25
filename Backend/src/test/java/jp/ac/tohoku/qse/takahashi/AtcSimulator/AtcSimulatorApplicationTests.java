193df5d8af2701cbf8b0ee6b6d87f1295984809f
charlie <62843186+charlie-to@users.noreply.github.com>
2024-07-25 12:14:13 +0900

    breaking update: go to java

---
+++ Backend/src/test/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/AtcSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.tohoku.qse.takahashi.AtcSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtcSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
