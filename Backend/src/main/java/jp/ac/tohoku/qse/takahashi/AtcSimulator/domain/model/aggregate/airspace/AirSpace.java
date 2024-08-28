8eca24dd3a6e8e9c6ff20572599272eda8052a38
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-08-28 17:04:41 +0900

    add airspace document comment

--- Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/domain/model/aggregate/airspace/AirSpace.java
+++ Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/domain/model/aggregate/airspace/AirSpace.java
@@ -2,10 +2,13 @@ package jp.ac.tohoku.qse.takahashi.AtcSimulator.domain.model.aggregate.airspace;
 
 import jp.ac.tohoku.qse.takahashi.AtcSimulator.domain.model.entity.aircraft.Aircraft;
 
+/**
+ * 空域を表すインターフェース
+ * 侵入禁止空域の判定や墜落等の処理を行う
+ */
 public interface AirSpace {
     public void addAircraft(Aircraft aircraft);
     public void removeAircraft(Aircraft aircraft);
 
     void NextStep();
-
 }
