8ba058ec6ca3523c3ff3e66d670dc6d9e27bfbf1
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-09-02 13:12:42 +0900

    add earth radius and knot to km

--- Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/config/constants/GlobalConstants.java
+++ Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/config/constants/GlobalConstants.java
@@ -1,5 +1,7 @@
 package jp.ac.tohoku.qse.takahashi.AtcSimulator.config.constants;
 
 public class GlobalConstants {
-    public static final int REFRESH_RATE_HZ = 1;
+    public static final int REFRESH_RATE = 1;
+    public static final double EARTH_RADIUS = 6378.1; //赤道半径 [km]
+    public static final double KNOTS_TO_KM_PER_HOUR = 1.852; //ノットからkm/hに変換するための係数　1ノット = 1.852 km/h
 }
