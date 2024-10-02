e9189bfaead708b8e4355b6d87a7c3dde2cab316
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-10-02 14:38:47 +0900

    add fix type

--- Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/domain/model/valueObject/Type/FixType.java
+++ Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/domain/model/valueObject/Type/FixType.java
@@ -1,5 +1,9 @@
 package jp.ac.tohoku.qse.takahashi.AtcSimulator.domain.model.valueObject.Type;
 
-public class FixType {
-    public String type; // FixType type(https://www.japa.or.jp/wp-content/uploads/2021/04/how_to_aeronautical_chart_2021.pdf )
+public enum FixType {
+    // FixType type(https://www.japa.or.jp/wp-content/uploads/2021/04/how_to_aeronautical_chart_2021.pdf )
+    TACAN,
+    DME,
+    VOR,
+    VOR_DME,
 }
