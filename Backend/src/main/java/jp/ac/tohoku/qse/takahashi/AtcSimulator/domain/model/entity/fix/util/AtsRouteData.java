6ea198e87fe6d35a3a77b1384eea012cb3d71804
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-10-03 10:03:26 +0900

    Delete unuse files

--- Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/domain/model/entity/fix/util/AtsRouteData.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.tohoku.qse.takahashi.AtcSimulator.domain.model.entity.fix.util;
-
-import java.util.List;
-
-public class AtsRouteData {
-    public List<Fix> fixes;
-    public List<List<String>> connections;
-}
--- Backend/src/main/java/jp/ac/tohoku/qse/takahashi/AtcSimulator/domain/model/entity/fix/util/Fix.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.tohoku.qse.takahashi.AtcSimulator.domain.model.entity.fix.util;
-
-public class Fix {
-    public String name;
-    public String type;
-    public double latitude;
-    public double longitude;
-}
