deb6c8a7bc8408b134c3e9af48b4cc1ec7930966
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2025-05-22 01:38:13 +0900

    Add link to Controller Page in Home component

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <a href="/operator">Go to Operator Page</a>
+      <a href="/operator">Go to Operator Page</a><br/>
+      <a href="/controller">Go to Controller Page</a>
     </div>
   );
 }
